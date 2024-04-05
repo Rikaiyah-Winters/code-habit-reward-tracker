d38a3d79db67ac3aaf1f38174d641f016118999e
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2024-04-04 21:57:48 -0500

    Changed class names for reward input

--- src/Components/RewardInput.js
+++ src/Components/RewardInput.js
@@ -3,11 +3,11 @@ import React from 'react';
 function RewardInput({ rewards, setRewards, list, handleSubmit, setPage }) {
 
     return (
-        <div className='reward-input'>
+        <div className='reward-list'>
             <h1>Please make a list of things you would like to reward yourself with for coding.</h1>
             <form onSubmit={handleSubmit}>
                 <input
-                    className='material-reward'
+                    className='reward-input'
                     value={rewards}
                     onChange={(e) => setRewards(e.target.value)} />
                 <button onClick={handleSubmit}>Submit Reward Ideas</button>
