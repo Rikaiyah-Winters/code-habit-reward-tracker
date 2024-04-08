2c1470a874e0b14624723578ffc39b8dadbe1380
Rikaiyah Winters <rikaiyahswinters@gmail.com>
2024-04-08 17:51:53 -0500

    Updated CSS

--- src/Components/HoursCoding.js
+++ src/Components/HoursCoding.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-function HoursCoding({decrementCount, count, incrementCount, alertOrOutput}) {
+function HoursCoding({ decrementCount, count, incrementCount, alertOrOutput }) {
     return (
         <div className='hours-coding'>
             <h1>How many hours did you code today?</h1>
-            <button disabled={count < 1}
-            onClick={decrementCount}>-</button>
-            <span>{count}</span>
-            <button onClick={incrementCount}>+</button>
-            <button onClick={alertOrOutput}>Submit</button>
+            <div className='count-number-div'>
+                <span className='count-number'>{count}</span>
+            </div>
+
+            <div className='count-buttons'>
+                <button className="de-increment-button" disabled={count < 1}
+                    onClick={decrementCount}>-</button>
+                <button className="de-increment-button" onClick={incrementCount}>+</button>
+            </div>
+            <button className="study-hours-submit-button" onClick={alertOrOutput}>Submit</button>
         </div>
     )
 }
